0aad52f98e8531cadc58cae1a5c356d980c4ba30
anubhav singh <anubhavsingh230@gmail.com>
2023-07-07 00:48:05 +0530

    simple function with perameter

--- fuctions.js
+++ fuctions.js
@@ -3,7 +3,16 @@
 function test() {
     let a = 5,
         b = 6
-    c = a + b
+    let c = a + b
     console.log(c)
 }
 test()
+    // function having two perameter and printing their addtion
+
+function add(a, b) {
+    let c = a + b
+    console.log(c)
+
+
+}
+add(5, 6) //passing 5,6 as  arguement
